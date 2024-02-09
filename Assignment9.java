71bc0e3fca9187270aed70a671cbf77199cc0401
jlobrien22 <jlobrien@oakland.edu>
2024-02-09 10:16:43 -0500

    commit

---
+++ Assignment9.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Assignment9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of Fibonacci numbers to compute: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Fibonacci Series:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1)
+            return n;
+        int a = 0, b = 1, sum;
+        for (int i = 2; i <= n; i++) {
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return b;
+    }
+}
+
